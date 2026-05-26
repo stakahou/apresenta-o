faa4bde9f9f87c793ecc3725542744b5bee9ccd4
Joshua <benrabi@gmail.com>
2026-05-26 14:43:19 -0300

    Update schema.ts

--- src/pages/SmartExploration/forms/schema.ts
+++ src/pages/SmartExploration/forms/schema.ts
@@ -3,4 +3,7 @@ import { z } from "zod";
 export const schema = z.object({
   type: z.string(),
   scenario: z.string(),
+  die: z.tuple([z.number(), z.number()]),
+  enca: z.tuple([z.number(), z.number()]),
+  sub: z.tuple([z.number(), z.number()]),
 });
